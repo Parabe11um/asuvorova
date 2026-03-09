751a0469c8df2abaf47deb8b3adf48beff26ca07
Parabe11um <r.a.bazhenoff@gmail.com>
2026-03-09 20:46:59 +0300

    prices-banner fixes

--- resources/views/layouts/partials/slider.blade.php
+++ resources/views/layouts/partials/slider.blade.php
@@ -15,9 +15,9 @@ before:content-[''] before:block before:absolute before:z-[1] before:w-full befo
                                 class="md:w-11/12 lg:w-8/12 xl:w-7/12 xxl:w-6/12 w-full flex-[0_0_auto]
 px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[2.8rem] leading-tight font-bold mb-4 !text-white">                                    Расслабьтесь и доверьтесь профессионалам</h2>
-                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-2s">
+                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-1s">
                                     Современные техники окрашивания, ухода и стиля — для идеального образа каждый день.</p>
-                                <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#"
+                                <div class="animate__animated animate__slideInUp animate__delay-1s"><a href="#"
                                                                                                        class="btn btn-lg btn-outline-white
           !rounded-none
           !text-white bg-[#ffffff]
@@ -44,9 +44,9 @@ px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 class="md:w-11/12 lg:w-8/12 xl:w-7/12 xxl:w-6/12 w-full flex-[0_0_auto]
 px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[2.8rem] leading-tight font-bold mb-4 !text-white">                                    Более 1000 клиентов уже доверили нам свою красоту</h2>
-                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-2s">
+                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-1s">
                                     Профессиональные мастера, качественные материалы и атмосфера заботы о каждом клиенте.</p>
-                                <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#"
+                                <div class="animate__animated animate__slideInUp animate__delay-1s"><a href="#"
                                                                                                        class="btn btn-lg btn-outline-white
           !rounded-none
           !text-white bg-[#ffffff]
@@ -73,9 +73,9 @@ px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 class="md:w-11/12 lg:w-8/12 xl:w-7/12 xxl:w-6/12 w-full flex-[0_0_auto]
 px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[2.8rem] leading-tight font-bold mb-4 !text-white">                                    Расслабьтесь и доверьтесь профессионалам</h2>
-                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-2s">
+                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-1s">
                                     Мы создаём пространство, где красота начинается с комфорта и внимания к деталям.</p>
-                                <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#"
+                                <div class="animate__animated animate__slideInUp animate__delay-1s"><a href="#"
                                                                                                        class="btn btn-lg btn-outline-white
           !rounded-none
           !text-white bg-[#ffffff]
@@ -102,9 +102,9 @@ px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 class="md:w-11/12 lg:w-8/12 xl:w-7/12 xxl:w-6/12 w-full flex-[0_0_auto]
 px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[2.8rem] leading-tight font-bold mb-4 !text-white">                                    Расслабьтесь и доверьтесь профессионалам</h2>
-                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-2s">
+                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-1s">
                                     Мы создаём пространство, где красота начинается с комфорта и внимания к деталям.</p>
-                                <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#"
+                                <div class="animate__animated animate__slideInUp animate__delay-1s"><a href="#"
                                                                                                        class="btn btn-lg btn-outline-white
           !rounded-none
           !text-white bg-[#ffffff]
@@ -131,9 +131,9 @@ px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 class="md:w-11/12 lg:w-8/12 xl:w-7/12 xxl:w-6/12 w-full flex-[0_0_auto]
 px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[2.8rem] leading-tight font-bold mb-4 !text-white">                                    Расслабьтесь и доверьтесь профессионалам</h2>
-                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-2s">
+                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-1s">
                                     Мы создаём пространство, где красота начинается с комфорта и внимания к деталям.</p>
-                                <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#"
+                                <div class="animate__animated animate__slideInUp animate__delay-1s"><a href="#"
                                                                                                        class="btn btn-lg btn-outline-white
           !rounded-none
           !text-white bg-[#ffffff]
@@ -160,9 +160,9 @@ px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 class="md:w-11/12 lg:w-8/12 xl:w-7/12 xxl:w-6/12 w-full flex-[0_0_auto]
 px-[15px] max-w-2xl mx-auto text-center self-start pt-32 md:pt-40">
                                 <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[2.8rem] leading-tight font-bold mb-4 !text-white">                                    Расслабьтесь и доверьтесь профессионалам</h2>
-                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-2s">
+                                <p class="lead text-[1.15rem] leading-normal !mb-7 !text-white animate__animated animate__slideInRight animate__delay-1s">
                                     Мы создаём пространство, где красота начинается с комфорта и внимания к деталям.</p>
-                                <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#"
+                                <div class="animate__animated animate__slideInUp animate__delay-1s"><a href="#"
                                                                                                        class="btn btn-lg btn-outline-white
           !rounded-none
           !text-white bg-[#ffffff]
